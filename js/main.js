b29aaf9de582ab80483e9a07dc09707418bc4a63
Yana <ten777yana@mail.ru>
2022-06-02 13:43:29 +0300

    correct the code according to the codeguide

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const initialRangeNumber = 2.2;
 const finalRangeNumber = 3.1;
-const presicion = 3;
+const presicionNumber = 3;
 
 function getRandomNumber (initialNumber, finalNumber, presicion = 2) {
   if (initialNumber < 0 || finalNumber < 0) {
-    console.log('Unvalid range');
     return;
   }
 
@@ -15,4 +14,4 @@ function getRandomNumber (initialNumber, finalNumber, presicion = 2) {
   return (Math.random() * (finalNumber - initialNumber) + initialNumber).toFixed(presicion);
 }
 
-getRandomNumber(initialRangeNumber, finalRangeNumber, presicion);
+getRandomNumber(initialRangeNumber, finalRangeNumber, presicionNumber);
